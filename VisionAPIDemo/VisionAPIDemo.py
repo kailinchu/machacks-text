c78b22ba62c4f5790688bdbee996e031f26e7bcd
Kailin <68765813+kailinchu@users.noreply.github.com>
2021-02-06 18:22:33 -0500

    fix path error and api

--- VisionAPIDemo/VisionAPIDemo.py
+++ VisionAPIDemo/VisionAPIDemo.py
@@ -1,17 +1,21 @@
 import os, io
 from google.cloud import vision_v1
-import pandas as pd
+## import pandas as pd
 
-os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = r'ServiceAccountToken.json'
+##os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = "ServiceAccountToken.json"
 
-client = vision_v1.ImageAnnotatorClient()
+#client = vision_v1.ImageAnnotatorClient()
+client = vision_v1.ImageAnnotatorClient.from_service_account_json(r"VisionAPIDemo/ServiceAccountToken.json")
 
-file_name = 'safety.jpg'
-FOLDER_PATH = r'C:\Users\16477\OneDrive\Desktop\VisionAPIDemo\Images\Text'
+image_file = 'IMG_8230.JPG'
+FOLDER_PATH = r'VisionAPIDemo/Images/Text'
+FILE_PATH = os.path.join(FOLDER_PATH, image_file)
 
-with io.open(FOLDER_PATH, 'rb') as image_file:
+with io.open(FILE_PATH, 'rb') as image_file:
     content = image_file.read()
 
 image = vision_v1.types.Image(content=content)
-
+# pylint: disable=no-member
 response = client.text_detection(image=image)
+docText = response.full_text_annotation.text
+print(docText)
