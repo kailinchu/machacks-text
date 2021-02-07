b516be4c293496a7a64b2ecfd8a6ed5e56a72f10
ACYau <aaronchyau@gmail.com>
2021-02-06 21:14:00 -0500

    Added a file upload function
    Post get request functions were added to the site.

--- Site/main.py
+++ Site/main.py
@@ -1,10 +1,35 @@
-from flask import Flask, render_template
+import os
+import VisionAPIDemo
+from flask import Flask, render_template, request, redirect, url_for
+from werkzeug.utils import secure_filename
 
+UPLOAD_FOLDER = r'VisionAPIDemo/Images/Text'
 app=Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 @app.route('/')
 def home():
     return render_template("home.html")
+
+"""
+@app.route('/')
+def text_content():
+    return VisionAPIDemo.text()
+"""
+
+@app.route('/', methods=['GET', 'POST'])
+def upload_file():
+    uploaded_file = request.files['file']
+    file_name()
+    if uploaded_file.filename != '':
+        #uploads to images folder
+        uploaded_file.save(os.path.join(app.config['UPLOAD_FOLDER'], secure_filename(uploaded_file.filename)))
+    return VisionAPIDemo.text()
+    #return redirect(url_for('home'))
+
+def file_name():
+    file_name = request.files['file']
+    return file_name.filename
 
 @app.route('/about/')
 def about():
