5a4b10999edef89decfb48d0f1d5e3f3099fde19
ACYau <aaronchyau@gmail.com>
2021-02-07 00:40:15 -0500

    No File Submission Error
    Resolved typerror when a file was not submitted and the user would hit the submit option. Website now displays text to prompt user to submit an image file when submission is left empty.

--- Site/main.py
+++ Site/main.py
@@ -24,9 +24,12 @@ def upload_file():
     if uploaded_file.filename != '':
         #uploads to images folder
         uploaded_file.save(os.path.join(app.config['UPLOAD_FOLDER'], secure_filename(uploaded_file.filename)))
-    text = VisionAPIDemo.text()
-    displaytext = "Your file says the following:"
-    return render_template("home.html",text=text, displaytext = displaytext)
+        text = VisionAPIDemo.text()
+        displaytext = "Your file says the following:"
+        return render_template("home.html",text=text, displaytext = displaytext)
+    else:
+        displaytext = "Please make sure you have submitted an image file (.jpg, .png)."
+        return render_template("home.html", displaytext = displaytext)
     ##return redirect(url_for('home'))
 
 def file_name():
